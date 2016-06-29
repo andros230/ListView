af9b7be83c3ed8d6f04cb5a0726014386b37971f
andros230 <andros230@126.com>
2016-06-29 16:31:38 +0800

    SimpleAdapter适配器

--- app/src/main/java/com/andros230/listview/MainActivity.java
+++ app/src/main/java/com/andros230/listview/MainActivity.java
@@ -4,28 +4,31 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends Activity {
-    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        list = new ListView(this);
-        list.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1,getdata()));
-        setContentView(list);
-    }
+        setContentView(R.layout.activity_main);
 
-    public List<String> getdata() {
-        List<String> data = new ArrayList<>();
-        data.add("测试数据1");
-        data.add("测试数据2");
-        data.add("测试数据3");
-        data.add("测试数据4");
-        return data;
+        ListView list = (ListView) findViewById(R.id.listView);
+
+        ArrayList<HashMap<String, String>> mylist = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            HashMap<String, String> map = new HashMap<>();
+            map.put("title" + i, "this is title " + i);
+            map.put("text", "this is text" + i);
+            mylist.add(map);
+        }
+
+        SimpleAdapter msche = new SimpleAdapter(this, mylist, R.layout.my_listitem, new String[]{"title", "text"}, new int[]{R.id.title, R.id.text});
+        list.setAdapter(msche);
     }
 }
