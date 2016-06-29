584417915f860595f0e876ac27a2e88a47b9295b
andros230 <andros230@126.com>
2016-06-29 17:48:07 +0800

    在ListView的Item里添加Button，并添加Button点击事件

--- app/src/main/java/com/andros230/listview/MyAdapter.java
+++ app/src/main/java/com/andros230/listview/MyAdapter.java
@@ -1,10 +1,13 @@
 package com.andros230.listview;
 
 import android.content.Context;
+import android.support.v4.widget.ViewDragHelper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
@@ -14,11 +17,11 @@ import java.util.List;
  */
 public class MyAdapter extends BaseAdapter {
     private List<String> list;
-    private Context context;
+    private LayoutInflater mInflater;
 
     public MyAdapter(List<String> list, Context context) {
         this.list = list;
-        this.context = context;
+        this.mInflater = LayoutInflater.from(context);
     }
 
     @Override
@@ -37,11 +40,33 @@ public class MyAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        View view2 = LayoutInflater.from(context).inflate(R.layout.adapter_custom_view, null);
-        TextView  textView = (TextView) view2.findViewById(R.id.textview33);
-        textView.setText(list.get(i));
-        System.out.println("--->" + i);
-        return view2;
+    public View getView(final int i, View view, ViewGroup viewGroup) {
+        ViewHolder holder;
+        if (view == null) {
+            holder = new ViewHolder();
+            view = mInflater.inflate(R.layout.adapter_custom_view, null);
+            holder.text = (TextView) view.findViewById(R.id.textview33);
+            holder.button = (Button) view.findViewById(R.id.button1);
+            view.setTag(holder);
+        } else {
+            holder = (ViewHolder) view.getTag();
+        }
+
+        holder.text.setText(list.get(i));
+
+        holder.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("---", i + "");
+            }
+        });
+        return view;
+
+    }
+
+
+    public class ViewHolder {
+        public TextView text;
+        public Button button;
     }
 }
